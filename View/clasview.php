c8c8c5dbd371035d9295842a574c50989b897b1c
Eloualid <eloualidgd@hotmail.com>
2019-10-30 10:38:04 +0100

    add beginnig of the project

---
+++ View/clasview.php
@@ -0,0 +1,30 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Form clas</title>
+</head>
+<body>
+
+<form action="" method="post">
+
+    Name:<br>
+    <input type="text" name="name" value="name">
+    <br>
+    Location:<br>
+    <input type="text" name="location" value="location">
+    <br><br>
+    <input type="submit" value="Submit">
+
+
+</form>
+
+</body>
+</html>

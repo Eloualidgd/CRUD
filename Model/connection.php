63f8637dac5104bcbb68506f1ae34ff39581f988
Eloualid <eloualidgd@hotmail.com>
2019-10-30 13:50:02 +0100

    clasview in orde

--- Model/connection.php
+++ Model/connection.php
@@ -5,16 +5,15 @@ declare(strict_types=1);
 function openConnection() {
     // Try to figure out what these should be for you
     $dbhost    = "localhost";
-    $dbuser    = "filip";
-    $dbpass    = "guest";
-    $db        = "becode";
+    $dbuser    = "Eloualid1";
+    $dbpass    = "Wactwoord123";
+    $db        = "crud";
 
-    function openConnection() {
-        $pdo = new PDO('mysql:host=localhost;dbname=becode', 'filip', 'guest');
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-
-        return $pdo;
-    }
-
-    $connection = openConnection();
+    // Try to understand what happens here
+    $pdo = new PDO('mysql:host='. $dbhost .';dbname='. $db, $dbuser, $dbpass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    // Why we do this here
+    return $pdo;
+}
+$connection = openConnection();
